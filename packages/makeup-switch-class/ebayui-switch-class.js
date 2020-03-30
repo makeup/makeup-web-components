185042cbfcc35d1ef63b696c72baf09fb07b3036
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2020-03-29 21:02:27 -0700

    Custom element now uses class under hood

--- packages/makeup-switch-class/ebayui-switch-class.js
+++ packages/makeup-switch-class/ebayui-switch-class.js
@@ -1,61 +1,36 @@
 class EbayUISwitchClass {
 
-    constructor(el) {
+    constructor(el, customElementMode) {
         this.el = el;
-        this._focusableElement = el.querySelector('.ebayui-switch__control');
 
         this._observer = new MutationObserver(this._onMutation);
         this._onClickListener = this._onClick.bind(this);
         this._onKeyDownListener = this._onKeyDown.bind(this);
 
-        this._observe();
-    }
-
-    set checked(isChecked) {
-        this._unobserve();
-        this._focusableElement.setAttribute('aria-checked', isChecked.toString());
-        this._observe();
-    }
-
-    get checked() {
-        return this._focusableElement.getAttribute('aria-checked') === 'true';
-    }
-
-    set disabled(isDisabled) {
-        this._unobserve();
-        this._focusableElement.setAttribute('aria-disabled', isDisabled.toString());
-        this._focusableElement.setAttribute('tabindex', isDisabled ? '-1' : '0');
-        this._observe();
-    }
-
-    get disabled() {
-        return this._focusableElement.getAttribute('aria-disabled') === 'true';
-    }
-
-    set labelledby(theId) {
-        this._unobserve();
-        this._focusableElement.setAttribute('aria-labelledby', theId);
-        this._observe();
-    }
-
-    toggle() {
-        if (!this.disabled) {
-            this.checked = !(this.checked);
+        if (!customElementMode) {
+            this._observeMutations();
+            this._observeEvents();
         }
     }
 
-    _observe() {
+    _observeMutations() {
         this._observer.observe(this._focusableElement, {
             attributes: true,
             childList: false,
             subtree: false
         });
+    }
+
+    _unobserveMutations() {
+        this._observer.disconnect();
+    }
+
+    _observeEvents() {
         this._focusableElement.addEventListener('click', this._onClickListener);
         this._focusableElement.addEventListener('keydown', this._onKeyDownListener);
     }
 
-    _unobserve() {
-        this._observer.disconnect();
+    _unobserveEvents() {
         this._focusableElement.removeEventListener('click', this._onClickListener);
         this._focusableElement.removeEventListener('keydown', this._onKeyDownListener);
     }
@@ -88,4 +63,41 @@ class EbayUISwitchClass {
             }
         }
     };
+
+    get _focusableElement() {
+        return this.el.querySelector('.ebayui-switch__control');
+    }
+
+    set checked(isChecked) {
+        this._unobserveMutations();
+        this._focusableElement.setAttribute('aria-checked', isChecked.toString());
+        this._observeMutations();
+    }
+
+    get checked() {
+        return this._focusableElement.getAttribute('aria-checked') === 'true';
+    }
+
+    set disabled(isDisabled) {
+        this._unobserveMutations();
+        this._focusableElement.setAttribute('aria-disabled', isDisabled.toString());
+        this._focusableElement.setAttribute('tabindex', isDisabled ? '-1' : '0');
+        this._observeMutations();
+    }
+
+    get disabled() {
+        return this._focusableElement.getAttribute('aria-disabled') === 'true';
+    }
+
+    set labelledby(theId) {
+        this._unobserveMutations();
+        this._focusableElement.setAttribute('aria-labelledby', theId);
+        this._observeMutations();
+    }
+
+    toggle() {
+        if (!this.disabled) {
+            this.checked = !(this.checked);
+        }
+    }
 }
--- packages/makeup-switch-element/ebayui-switch-element.js
+++ packages/makeup-switch-element/ebayui-switch-element.js
@@ -8,23 +8,45 @@ class EbayUISwitchElement extends HTMLElement {
         ];
     }
 
+    connectedCallback(e) {
+        this.removeAttribute('placeholder');
+        this.model._observeEvents();
+    }
+
+    disconnectedCallback(e) {
+        this.model._unobserveEvents();
+    }
+
+    attributeChangedCallback(attr, oldVal, newVal) {
+        switch(attr) {
+            case 'checked':
+                this.model.checked = (newVal === null) ? false : true;
+                break;
+            case 'disabled':
+                this.model.disabled = (newVal === null) ? false : true;
+                break;
+            case 'labelledby':
+                this.model.labelledby = newVal;
+                break;
+            default:
+                break;
+        }
+    }
+
     constructor() {
         super();
-
-        this._onClickListener = this._onClick.bind(this);
-        this._onKeyDownListener = this._onKeyDown.bind(this);
 
         const shadowRoot = this.attachShadow({ mode: 'open' });
 
         shadowRoot.innerHTML = `
           <style>
-              .switch {
+              .ebayui-switch {
                 display: inline-flex;
                 height: 40px;
                 position: relative;
                 vertical-align: middle;
               }
-              .switch__button {
+              .ebayui-switch__button {
                 align-self: center;
                 background: gray none repeat scroll 0 0;
                 background-color: #767676;
@@ -37,7 +59,7 @@ class EbayUISwitchElement extends HTMLElement {
                 transition: left 0.15s ease-out 0s;
                 width: 40px;
               }
-              .switch__button::after {
+              .ebayui-switch__button::after {
                 background: white none repeat scroll 0 0;
                 background-color: #fff;
                 border-radius: 50%;
@@ -51,7 +73,7 @@ class EbayUISwitchElement extends HTMLElement {
                 transition: left 0.15s ease-out 0s;
                 width: 18px;
               }
-              .switch__control {
+              .ebayui-switch__control {
                 height: 24px;
                 left: 0;
                 margin: 0;
@@ -62,104 +84,27 @@ class EbayUISwitchElement extends HTMLElement {
                 width: 40px;
                 z-index: 1;
               }
-              .switch__control:focus + .switch__button {
+              .ebayui-switch__control:focus + .ebayui-switch__button {
                 outline: 1px dotted #767676;
               }
-              .switch__control[aria-disabled="true"] + .switch__button {
+              .ebayui-switch__control[aria-disabled="true"] + .ebayui-switch__button {
                 background-color: #c7c7c7;
               }
-              .switch__control[aria-checked="true"] + .switch__button::after {
+              .ebayui-switch__control[aria-checked="true"] + .ebayui-switch__button::after {
                 left: 19px;
               }
-              .switch__control:not([aria-disabled="true"])[aria-checked="true"] + .switch__button {
+              .ebayui-switch__control:not([aria-disabled="true"])[aria-checked="true"] + .ebayui-switch__button {
                 background-color: #3665f3;
               }
           </style>
 
-          <span class="switch">
-            <span class="switch__control" role="switch" tabindex="0" aria-checked="false"></span>
-            <span class="switch__button"></span>
+          <span class="ebayui-switch">
+            <span class="ebayui-switch__control" role="switch" tabindex="0" aria-checked="false"></span>
+            <span class="ebayui-switch__button"></span>
           </span>
         `;
-    }
 
-    get _focusableElement() {
-        return this.shadowRoot.querySelector('.switch__control');
-    }
-
-    set checked(isChecked) {
-        this._focusableElement.setAttribute('aria-checked', isChecked.toString());
-    }
-
-    get checked() {
-        return this._focusableElement.getAttribute('aria-checked') === 'true';
-    }
-
-    set disabled(isDisabled) {
-        this._focusableElement.setAttribute('aria-disabled', isDisabled.toString());
-        this._focusableElement.setAttribute('tabindex', isDisabled ? '-1' : '0');
-    }
-
-    get disabled() {
-        return this._focusableElement.getAttribute('aria-disabled') === 'true';
-    }
-
-    set labelledby(theId) {
-        this._focusableElement.setAttribute('aria-labelledby', theId);
-    }
-
-    attributeChangedCallback(attr, oldVal, newVal) {
-        switch(attr) {
-            case 'checked':
-                this.checked = (newVal === null) ? false : true;
-                break;
-            case 'disabled':
-                this.disabled = (newVal === null) ? false : true;
-                break;
-            case 'labelledby':
-                this.labelledby = newVal;
-                break;
-            default:
-                break;
-        }
-    }
-
-    connectedCallback(e) {
-        this.removeAttribute('placeholder');
-        this._focusableElement.addEventListener('click', this._onClickListener);
-        this._focusableElement.addEventListener('keydown', this._onKeyDownListener);
-    }
-
-    disconnectedCallback(e) {
-        this._focusableElement.removeEventListener('click', this._onClickListener);
-        this._focusableElement.removeEventListener('keydown', this._onKeyDownListener);
-    }
-
-    toggle() {
-        if (!this.disabled) {
-            this.checked = !(this.checked);
-        }
-    }
-
-    _onKeyDown(e) {
-        switch (e.keyCode) {
-            case 32:
-                e.preventDefault();
-                this.toggle();
-                break;
-            case 37:
-                this.checked = false;
-                break;
-            case 39:
-                this.checked = true;
-                break;
-            default:
-                break;
-        }
-    }
-
-    _onClick(e) {
-        this.toggle();
+        this.model = new EbayUISwitchClass(this.shadowRoot.querySelector('.ebayui-switch'), true);
     }
 }
 
