91c3756055b10053cc7d5f78722a43e133023d44
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2020-03-30 09:42:20 -0700

    Removed @import

--- packages/makeup-switch-element/switch-element.js
+++ packages/makeup-switch-element/switch-element.js
@@ -40,7 +40,66 @@ class MakeupUISwitchElement extends HTMLElement {
 
         this.shadowRoot.innerHTML = `
           <style>
-              @import '../makeup-switch-style/switch.css';
+              .makeup-switch {
+                display: inline-flex;
+                height: 40px;
+                position: relative;
+                vertical-align: middle;
+              }
+              .makeup-switch__button {
+                align-self: center;
+                background: gray none repeat scroll 0 0;
+                background-color: #767676;
+                background-color: var(--switch-background-color-unchecked, #767676);
+                border-radius: 400px;
+                color: transparent;
+                display: inline-block;
+                height: 24px;
+                position: relative;
+                text-indent: 100%;
+                transition: left 0.15s ease-out 0s;
+                width: 40px;
+              }
+              .makeup-switch__button::after {
+                background: white none repeat scroll 0 0;
+                background-color: #fff;
+                background-color: var(--switch-foreground-color, #fff);
+                border-radius: 50%;
+                content: "";
+                display: block;
+                height: 18px;
+                left: 3px;
+                position: absolute;
+                top: 3px;
+                transform: translate3d(0, 0, 0);
+                transition: left 0.15s ease-out 0s;
+                width: 18px;
+              }
+              .makeup-switch__control {
+                height: 24px;
+                left: 0;
+                margin: 0;
+                opacity: 0;
+                padding: 0;
+                position: absolute;
+                top: 8px;
+                width: 40px;
+                z-index: 1;
+              }
+              .makeup-switch__control:focus + .makeup-switch__button {
+                outline: 1px dotted #767676;
+              }
+              .makeup-switch__control[aria-disabled="true"] + .makeup-switch__button {
+                background-color: #c7c7c7;
+                background-color: var(--switch-background-color-disabled, #c7c7c7);
+              }
+              .makeup-switch__control[aria-checked="true"] + .makeup-switch__button::after {
+                left: 19px;
+              }
+              .makeup-switch__control:not([aria-disabled="true"])[aria-checked="true"] + .makeup-switch__button {
+                background-color: #3665f3;
+                background-color: var(--switch-background-color-checked, #3665f3);
+              }
           </style>
 
           <span class="makeup-switch">
