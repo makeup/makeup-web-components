3029d566961b11787a31007f991f973961a1c143
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2020-03-29 21:41:29 -0700

    Rename files

--- packages/makeup-switch-class/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(e) {
-    const switchClasses = document.querySelectorAll('.makeup-switch');
-
-    switchClasses.forEach(function(e, i) {
-        const widget = new MakeupUISwitchClass(e);
-    });
-};
--- packages/makeup-switch-element/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(e) {
-    const switchEls = document.querySelectorAll('makeup-switch');
-
-    switchEls.forEach(function(e, i) {
-        console.log(e, i);
-    });
-};
